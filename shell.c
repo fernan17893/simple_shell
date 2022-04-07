316d99bf0b41654e692031c3da8c2a35ed884446
Icarus399 <3530@holbertonschool.com>
2022-04-07 16:39:46 +0000

    adding files for shell.c

--- shell.c
+++ shell.c
@@ -8,8 +8,8 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	pid_t child;
-	char *command[16], *tok, *lineptr = NULL; // Not going to use commands with more than 15 words; extra room for NULL term
-	size_t i, n; // Number of bytes read into lineptr (mallocated internally by getline)
+	char *command[16], *tok, *lineptr = NULL;
+	size_t i, n;
 	int status;
 
 	while (1)
@@ -17,15 +17,15 @@ int main(int argc, char *argv[], char *envp[])
 		printf("$ ");
 		if (getline(&lineptr, &n, stdin) == -1)
 			break;
-		tok = strtok(lineptr, " \t\n\r"); // First call to strtok stores the line in a static variable; second arg is delimiters
-		for (i = 0; i < 16 && tok != NULL; i++) // strtok returns NULL once there are no more tokens
+		tok = strtok(lineptr, " \t\n\r");
+		for (i = 0; i < 16 && tok != NULL; i++)
 		{
 			command[i] = tok;
 			tok = strtok(NULL, " \t\n\r");7
 		}
-		command[i] = NULL; // `execve' expects a NULL terminator from both the command and the environment; this assumes i < 15...
+		command[i] = NULL;
 		child = fork();
-		if (child == 0) // No error checking; too close to being not a "snippet"
+		if (child == 0)
 		{
 			if (execve(command[0], command, envp))
 			{
@@ -33,7 +33,7 @@ int main(int argc, char *argv[], char *envp[])
 				exit(EXIT_FAILURE);
 			}
 		}
-		if (child > 0) // Successful forks return positive process id's the parent
+		if (child > 0)
 			wait(&status);
 	}
 	putchar('\n');
