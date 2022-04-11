6e2c45103b2a8725f83d8193467cb98e62dc7b26
Icarus399 <3530@holbertonschool.com>
2022-04-11 14:56:55 +0000

    renaming files and adding others

--- shell.c
+++ shell.c
@@ -5,38 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[], char *envp[])
+int main 
 {
-	pid_t child;
-	char *command[16], *tok, *lineptr = NULL;
-	size_t i, n;
-	int status;
 
-	while (1)
-	{
-		printf("$ ");
-		if (getline(&lineptr, &n, stdin) == -1)
-			break;
-		tok = strtok(lineptr, " \n");
-		for (i = 0; i < 16 && tok != NULL; i++)
-		{
-			command[i] = tok;
-			tok = strtok(NULL, " \n");
-		}
-		command[i] = NULL;
-		child = fork();
-		if (child == 0)
-		{
-			if (execve(command[0], command, envp))
-			{
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-		}
-		if (child > 0)
-			wait(&status);
-	}
-	putchar('\n');
-	free(lineptr);
-	exit(status);
-}
