e9f85f0746c52660aeccde6072d44868d1807b2a
hoodrich-fern <fernan17893@gmail.com>
2022-04-12 15:40:15 +0000

    main up

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+
+
+#endif
