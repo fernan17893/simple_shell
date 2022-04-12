b5a588e15dabf738a332e736c3a2fe451bc8f792
Icarus399 <3530@holbertonschool.com>
2022-04-12 21:09:56 +0000

    adding more of the functions used

---
+++ MoreFunctions.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * _printenv - prints environment
+ * @envp: environment
+ * Return: 0 if succesful
+ */
+
+int _printenv(char *envp[])
+{
+	int i, n;
+
+	for (i = 0; envp[i] != NULL; i++)
+	{
+		n = _strlen(envp[i]);
+		write(1, envp[i], n);
+		_putchar('\n');
+	}
+	return (0);
+}
+
+/**
+ * _free_double - frees a double pointer and sets it to NULL
+ * @token: double pointer to free
+ * Return: 0 if succesful
+ */
+
+int _free_double(char **token)
+{
+	if (token != NULL)
+	{
+		free(token[0]);
+		token[0] = NULL;
+		free(token);
+		token = NULL;
+	}
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _check_program - checks if program is valid before passing to execve
+ * @program: program to validate
+ * Return: 0 if valid, -1 otherwise
+ */
+
+int _check_program(char *program)
+{
+	struct stat status;
+
+	if (stat(program, &status) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
